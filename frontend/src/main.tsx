5754ff753a4cd4ca18e25494de77e1b8832c32fe
AndreiTsN <andrei.ts.tech@gmail.com>
2025-11-19 20:49:30 +0100

    fix: add Buffer polyfill for production build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { WalletConnectionProvider } from "./WalletConnectionProvider";
+import { Buffer } from "buffer";
+
+
+// @ts-ignore
+window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
